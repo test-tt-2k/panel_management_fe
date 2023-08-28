e20a9f67ed9c4defb882bbe09230b87d5907f343
dinhphamcanh <dinhpc@solashi.com>
2023-08-28 20:47:13 +0700

    fix: domain

--- src/lib/api/request.js
+++ src/lib/api/request.js
@@ -1,6 +1,6 @@
 import Axios from "axios";
 
-const baseURL = "http://localhost:8800/api/v1/";
+const baseURL = "http://45.32.103.75:8800/api/v1/";
 
 export const request = Axios.create({
   baseURL,
